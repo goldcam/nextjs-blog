c12ec1334064162dd29df341ea73efdca08a8141
Cameron Townsend <cameron.townsend@wildtangent.com>
2022-01-21 13:18:29 -0800

    blog ready for commit

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+// If you want to customize the <html> tag, for example to add the lang attribute, you can do so by creating a pages/_document.js file.
+
+//https://nextjs.org/docs/advanced-features/custom-document
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
